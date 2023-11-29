c1b1a5c23d8dc68fa2d586e18a44b3e49973530a
starboyeyadd <eyaid.hesham07@gmail.com>
2023-11-29 06:36:21 -0800

    updated

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# Create a file using puppet
+# create a file with permissions and a content
 file { '/tmp/school':
-  ensure  => file,
+  ensure  => 'file',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
+  content => 'I love Puppet', # This is another comment
 }
