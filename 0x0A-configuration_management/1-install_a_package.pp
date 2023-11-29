5ce7a35e86e3d60d6a6e8a923c98018ffeae50e8
starboyeyadd <eyaid.hesham07@gmail.com>
2023-11-29 06:32:49 -0800

    updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
-# Install a package
-
-package { 'flask' :
-	ensure   => '2.1.0',
-	name	=> 'flask',
-	provider	=>'pip3'
+# Installs puppet-lint
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => gem,
 }
